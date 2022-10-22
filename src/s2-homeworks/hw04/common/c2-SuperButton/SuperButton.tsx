7dac32696476711dd931de1a07649223ad908ae3
16player <191292km@gmail.com>
2022-10-22 13:25:38 +0300

    update

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -19,9 +19,9 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
 ) => {
     const finalClassName = s.button
         + (disabled && s.secondary
-                ? s.disabled && s.disabled
+                ? s.disabled
                 : xType === 'red'
-                    ? s.red
+                    ? s.red && s.secondary
         + (className ? ' ' + className : '') : s.red) // задачка на смешивание классов
 
     return (
